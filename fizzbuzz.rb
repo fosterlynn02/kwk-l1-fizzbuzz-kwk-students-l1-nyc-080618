92d2d27fbfb92ebbc6aaff53bf06e5e037c64173
fosterlynn02 <fosterlynnboulin@gmail.com>
2018-08-07 18:32:39 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+x = 45
+
+
+  if x % 3 == 0 && x % 5 == 0
+      print "fizzbuzz"
+      elsif x % 5 == 0
+    print "buzz"
+      elsif x % 3 == 0
+      print "fizz"
+    
+  end 
+  
---
+++ loops.rb
@@ -0,0 +1,9 @@
+def hunry_Victoria
+  pizza = 0
+  while pizza < 50 
+  puts "I AM CRAVING PIZZA"
+  pizza += 1 
+end 
+end 
+
+hunry_Victoria
